55448460e91e8f749b7648c1d9b00bef5d4f0a20
skoohgoli <zaktan.skakdi@gmail.com>
2016-06-08 13:47:13 -0700

    Update Lesson7Assignment.java

--- Lesson7Assignment.java
+++ Lesson7Assignment.java
@@ -10,12 +10,20 @@ public class SelectionSorter {
         }
         int length = input.length;
         for (int i = 0; i < length; i++) {
+            int minIndex = i;
+            
+            //find the smallest number in the sub-array
             for (int j = i; j < length; j++) {
-                if (input[j] < input[i]) {
-                    int temp = input[i];
-                    input[i] = input[j];
-                    input[j] = temp;
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
                 }
+            }
+            
+            //if the smallest number is not at input[i], swap so that it is
+            if (minIndex != i) {
+                int temp = input[i];
+                input[i] = input[minIndex];
+                input[minIndex] = temp;
             }
         }
         return input;
